5f0947048b5368f238c81385ecd3d4a9aed5f8e2
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2017-02-24 11:27:55 -0800

    Got a jump working properly in a test scene
    Yep, jumping works if given a time to the apex and a height. It bothers
    me that it's a little bit inaccurate on the timing, but it's a start.
    I'd like to make the final jump method more customizable, ie: time to
    apex, hang time, time from apex to ground, height, etc. I'll look into
    making the jump options feel good.

---
+++ PlatformDev/PlatformDev/Assets/Scripts/JumpEquationTester.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpEquationTester : MonoBehaviour 
+{
+	public float jumpHeight;
+	public float jumpTimeToApex;
+	public float gravity;
+
+	private float jumpTimer;
+	private bool isJumping;
+	private Vector2 velocity;
+
+	void Start()
+	{
+		isJumping = false;
+		jumpTimer = 0.0f;
+		velocity = Vector2.zero;
+		jumpTimeToApex *= 51.0f;
+	}
+
+	void FixedUpdate()
+	{
+		//Update velocity...
+		//velocity = Vector2.zero;
+		//if (!isJumping)
+			//velocity.y += gravity; //Apply gravity
+		//else
+		velocity.y += -((2.0f * jumpHeight) / jumpTimeToApex) / jumpTimeToApex;
+
+		if (!isJumping) //Jump state stuff
+		{
+			isJumping = true;
+			jumpTimer = 0.0f;
+
+			//Set to initial velocity...
+			velocity.y = (2.0f * jumpHeight) / jumpTimeToApex;
+		}
+
+		if (isJumping) //Jump velocity stuff
+		{
+			//float initialVelocity = 0.0f; //This is the initial velocity of the jump.
+			//initialVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+			//initialVelocity = (2.0f * jumpHeight) / jumpTimeToApex;
+			//float tempGravity = -initialVelocity / jumpTimeToApex;
+
+			jumpTimer += Time.deltaTime;
+			//velocity.y += -2.0f * (jumpTimer - jumpTimeToApex);
+			//velocity.y += jumpHeight - Mathf.Pow(jumpTimer - jumpTimeToApex, 2.0f);
+			//velocity.y += initialVelocity + gravity * (jumpTimer);
+			//velocity.y += initialVelocity - (initialVelocity / jumpHeight) * (jumpTimer);
+
+			Debug.Log ("Jumping... P :: " + transform.position.y + " V :: " + velocity.y + " T :: " + jumpTimer);
+		}
+		//End of velocity updates.
+
+		//Keep the jumper from falling off-screen...
+		if (transform.position.y <= 0)
+		{
+			transform.position = new Vector3 (transform.position.x, 0.0f, transform.position.z);
+			if(velocity.y < 0)
+			{
+				velocity.y = 0.0f;
+				isJumping = false; //Reset jumping state when we hit the "ground".
+			}
+		}
+
+		//Apply velocity...
+		transform.position += (Vector3)velocity;// * Time.deltaTime;
+	}
+}
