dc4488773ded37723489d92cf3d71d9d9d38e048
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2017-02-25 14:42:23 -0800

    Cleaned up the code a bit
    Just cleaned up and re-organized the jump code a little bit. No
    functional changes.

--- PlatformDev/PlatformDev/Assets/Scripts/JumpEquationTester.cs
+++ PlatformDev/PlatformDev/Assets/Scripts/JumpEquationTester.cs
@@ -9,72 +9,64 @@ public class JumpEquationTester : MonoBehaviour
 	public float jumpDistance_AfterApex;
 	public float playerSpeed;
 
-	private float jumpTimer;
 	private bool isJumping;
-	private float gravity;
-	private float initialVelocity;
-	private Vector2 velocity;
 	private bool jumpReleasedEarly;
+
+	private float gravity;
+
+	private Vector2 velocity;
+	private float initialVelocity;
 
 	void Start()
 	{
 		isJumping = false;
-		jumpTimer = 0.0f;
 		velocity = Vector2.zero;
 
 		jumpReleasedEarly = false;
 	}
 
-	void Update()
+	void Update() //State and input stuff...
 	{
 		//Movement...
 		velocity.x = Input.GetAxisRaw("Horizontal") * playerSpeed;
-		//velocity.x = playerSpeed; //For testing purposes.
 
 		if (Input.GetKeyDown(KeyCode.Space) && !isJumping) //Initiating a jump...
 		{
 			isJumping = true;
-			jumpTimer = 0.0f;
 
-			//Set to initial velocity...
+			//Set velocity to initial velocity...
 			initialVelocity = (2.0f * maxJumpHeight * playerSpeed) / (jumpDistance_BeforeApex);
 			velocity.y = initialVelocity;
 		}
-		if (Input.GetKeyUp (KeyCode.Space) && isJumping && !jumpReleasedEarly && velocity.y > 0) //Releasing jump key during a jump...
+		if (Input.GetKeyUp (KeyCode.Space) && isJumping && velocity.y > 0) // && !jumpReleasedEarly && velocity.y > 0) //Releasing jump key during a jump...
 		{
 			jumpReleasedEarly = true;
-			Debug.Log ("Jump Released Early :: " + jumpReleasedEarly);
-			if (transform.position.y <= minJumpHeight) //If we haven't reached the minimum jump height...
-			{
-				//Debug.Log ("Released :: Before Min Apex");
-				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * minJumpHeight);
-			}
-			else //If we're between min and max jump heights...
-			{
-				//Debug.Log ("Released :: After Min Apex");
-				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * (transform.position.y - minJumpHeight)); 
-				//Transform position here is representative of the current jumped height, not the actual position of the player.
-			}
 		}
 	}
 
-	void FixedUpdate()
+	void FixedUpdate() //Physics stuff...
 	{
-		//Time.timeScale = .25f;
-		if (isJumping && velocity.y < 0)// && !jumpReleasedEarly) 
+		if (isJumping && velocity.y < 0) //Use the gravity for max jump height. (Default)
 		{
 			//Post-apex, pre-landing gravity.
-			gravity = (-(2.0f * maxJumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_AfterApex, 2.0f);
+			gravity = (-(2.0f * maxJumpHeight) * Mathf.Pow (playerSpeed, 2.0f)) / Mathf.Pow (jumpDistance_AfterApex, 2.0f);
 		} 
-		else if (!jumpReleasedEarly)
+		else if (isJumping && jumpReleasedEarly && velocity.y > 0) //Use gravity based on when the jump button was released. (On early release)
+		{
+			if (transform.position.y <= minJumpHeight) //Below minimum jump height...
+			{
+				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * minJumpHeight);
+			}
+			else //Between min and max jump heights...
+			{
+				//Transform position here is representative of the current jumped height, not the actual position of the player.
+				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * (transform.position.y - minJumpHeight)); 
+			}
+		}
+		else if (!jumpReleasedEarly) //Use normal gravity. (Not associated with jumping)
 		{
 			//Normal gravity.
 			gravity = (-(2.0f * maxJumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_BeforeApex, 2.0f);
-		}
-
-		if (isJumping) //Jump velocity stuff
-		{
-			jumpTimer += Time.deltaTime;
 		}
 
 		//Keep the jumper from falling off-screen...
@@ -90,14 +82,11 @@ public class JumpEquationTester : MonoBehaviour
 		}
 
 		//Apply velocity...
-
-		//Modified velocity verlet method of interpolation (Broken)
-		//velocity *= Time.deltaTime;
-		//transform.position += ((Vector3)velocity * Time.deltaTime) + new Vector3(0.0f, (0.5f * gravity * Time.deltaTime * Time.deltaTime), 0.0f);
 		velocity.y += gravity; ///Apply gravity.
 		transform.position += (Vector3)velocity;// * Time.deltaTime;
 	}
 
+	//For debugging and testing...
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
