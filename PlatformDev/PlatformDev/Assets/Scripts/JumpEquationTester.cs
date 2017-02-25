024241b7a91ee36e582f26e52d4ce183d5fe3c09
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2017-02-25 13:15:46 -0800

    Implemented Variable Jump Height
    You can now define a minimum and maximum height for the jump. Next up is
    double jumping, which may or may not have variable heights applied to it
    as well. In the future, I may make the variable jump height based on how
    long you hold the button down instead of the position in the jump where
    the jump key is released.

--- PlatformDev/PlatformDev/Assets/Scripts/JumpEquationTester.cs
+++ PlatformDev/PlatformDev/Assets/Scripts/JumpEquationTester.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class JumpEquationTester : MonoBehaviour 
 {
-	public float jumpHeight;
+	public float maxJumpHeight;
+	public float minJumpHeight;
 	public float jumpDistance_BeforeApex;
 	public float jumpDistance_AfterApex;
 	public float playerSpeed;
@@ -11,65 +12,70 @@ public class JumpEquationTester : MonoBehaviour
 	private float jumpTimer;
 	private bool isJumping;
 	private float gravity;
+	private float initialVelocity;
 	private Vector2 velocity;
+	private bool jumpReleasedEarly;
 
 	void Start()
 	{
 		isJumping = false;
 		jumpTimer = 0.0f;
 		velocity = Vector2.zero;
-		Application.targetFrameRate = 10;
+
+		jumpReleasedEarly = false;
 	}
 
-	void FixedUpdate()
+	void Update()
 	{
-		Debug.Log (1.0f / Time.deltaTime);
-		//Update velocity...
-		//velocity = Vector2.zero;
-		//if (!isJumping)
-			//velocity.y += gravity; //Apply gravity
-		//else
-		if (isJumping && velocity.y < 0) 
-		{
-			gravity = (-(2.0f * jumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_AfterApex, 2.0f);
-		} 
-		else 
-		{
-			gravity = (-(2.0f * jumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_BeforeApex, 2.0f);
-		}
-
-		velocity.y += gravity;
-		//velocity.y += -((2.0f * jumpHeight) / (jumpTimeToApex / Time.fixedDeltaTime)) / (jumpTimeToApex / Time.fixedDeltaTime);
-
 		//Movement...
-		//velocity.x = Input.GetAxisRaw("Horizontal") * playerSpeed;
-		velocity.x = playerSpeed;
+		velocity.x = Input.GetAxisRaw("Horizontal") * playerSpeed;
+		//velocity.x = playerSpeed; //For testing purposes.
 
-		if (!isJumping) //Jump state stuff
+		if (Input.GetKeyDown(KeyCode.Space) && !isJumping) //Initiating a jump...
 		{
 			isJumping = true;
 			jumpTimer = 0.0f;
 
 			//Set to initial velocity...
-			velocity.y = (2.0f * jumpHeight * playerSpeed) / (jumpDistance_BeforeApex);
+			initialVelocity = (2.0f * maxJumpHeight * playerSpeed) / (jumpDistance_BeforeApex);
+			velocity.y = initialVelocity;
+		}
+		if (Input.GetKeyUp (KeyCode.Space) && isJumping && !jumpReleasedEarly && velocity.y > 0) //Releasing jump key during a jump...
+		{
+			jumpReleasedEarly = true;
+			Debug.Log ("Jump Released Early :: " + jumpReleasedEarly);
+			if (transform.position.y <= minJumpHeight) //If we haven't reached the minimum jump height...
+			{
+				//Debug.Log ("Released :: Before Min Apex");
+				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * minJumpHeight);
+			}
+			else //If we're between min and max jump heights...
+			{
+				//Debug.Log ("Released :: After Min Apex");
+				gravity = -(Mathf.Pow(initialVelocity, 2.0f)) / (2.0f * (transform.position.y - minJumpHeight)); 
+				//Transform position here is representative of the current jumped height, not the actual position of the player.
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		//Time.timeScale = .25f;
+		if (isJumping && velocity.y < 0)// && !jumpReleasedEarly) 
+		{
+			//Post-apex, pre-landing gravity.
+			gravity = (-(2.0f * maxJumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_AfterApex, 2.0f);
+		} 
+		else if (!jumpReleasedEarly)
+		{
+			//Normal gravity.
+			gravity = (-(2.0f * maxJumpHeight) * Mathf.Pow(playerSpeed, 2.0f)) / Mathf.Pow(jumpDistance_BeforeApex, 2.0f);
 		}
 
 		if (isJumping) //Jump velocity stuff
 		{
-			//float initialVelocity = 0.0f; //This is the initial velocity of the jump.
-			//initialVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
-			//initialVelocity = (2.0f * jumpHeight) / jumpTimeToApex;
-			//float tempGravity = -initialVelocity / jumpTimeToApex;
-
 			jumpTimer += Time.deltaTime;
-			//velocity.y += -2.0f * (jumpTimer - jumpTimeToApex);
-			//velocity.y += jumpHeight - Mathf.Pow(jumpTimer - jumpTimeToApex, 2.0f);
-			//velocity.y += initialVelocity + gravity * (jumpTimer);
-			//velocity.y += initialVelocity - (initialVelocity / jumpHeight) * (jumpTimer);
-
-			//Debug.Log ("Jumping... P :: " + transform.position.y + " V :: " + velocity.y + " T :: " + jumpTimer);
 		}
-		//End of velocity updates.
 
 		//Keep the jumper from falling off-screen...
 		if (transform.position.y + velocity.y <= 0)
@@ -79,12 +85,23 @@ public class JumpEquationTester : MonoBehaviour
 			{
 				velocity.y = 0.0f;
 				isJumping = false; //Reset jumping state when we hit the "ground".
+				jumpReleasedEarly = false;
 			}
 		}
 
 		//Apply velocity...
+
+		//Modified velocity verlet method of interpolation (Broken)
 		//velocity *= Time.deltaTime;
 		//transform.position += ((Vector3)velocity * Time.deltaTime) + new Vector3(0.0f, (0.5f * gravity * Time.deltaTime * Time.deltaTime), 0.0f);
+		velocity.y += gravity; ///Apply gravity.
 		transform.position += (Vector3)velocity;// * Time.deltaTime;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (new Vector3 (transform.position.x, minJumpHeight, transform.position.z), 0.125f);
+		Gizmos.DrawWireSphere (new Vector3 (transform.position.x, maxJumpHeight, transform.position.z), 0.125f);
 	}
 }
